9ef0c181ecf6ff1c7c19e4515627916c5e542828
zkq <835287873@qq.com>
2019-11-03 18:25:14 +0800

    完善了Gallery App

--- gallery/models.py
+++ gallery/models.py
@@ -4,4 +4,11 @@ from django.db import models
 
 # modeles 要在admin里注册
 class Gallery(models.Model):
-    description=models.CharField(max_length=100)
+    # CharField--字段字符 BigIntegerField--大整数字段 ImageField--图像字段
+    description=models.CharField(default='在这里写作品简介',max_length=100)
+    image = models.ImageField(default='default.png',upload_to='images/')
+    title=models.CharField(default='作品标题',max_length=50)
+    
+    def __str__(self):
+        return self.title
+
